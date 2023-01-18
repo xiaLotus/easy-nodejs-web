197efe650ba06034b2a5d65fa0aa8968fe198dce
xiaLotus <116232069+xiaLotus@users.noreply.github.com>
2023-01-18 20:47:14 +0800

    save

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # easy-nodejs-web
+
+
+##### use the cmd
+```cmd
+node app.js
+```
+
+and open the web on
+
+http://127.0.0.1:8080/
---
+++ app.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type': "text/plain"});
+    response.write('hello!');
+    response.end();
+}).listen(8080)
